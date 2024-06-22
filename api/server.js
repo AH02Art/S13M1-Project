ae58efb4204e8007d31e4dbf1069a199ab003b45
Alex Handley <smithvillescholarsah@gmail.com>
2024-06-21 22:23:57 -0500

    S13M1-Project completed

--- api/server.js
+++ api/server.js
@@ -3,6 +3,28 @@ const User = require("./users/model.js");
 
 const app = express();
 app.disable("x-powered-by");
+app.use(express.json());
+
+app.post("/api/users", (request, response) => {
+    const { name, bio } = request.body;
+    console.log("POST request body: ", request.body);
+    User.insert({ name, bio })
+    .then((data) => {
+        if (!data.name || !data.bio) {
+            response.status(400).json({
+                message: "Please provide name and bio for the user"
+            });
+        } else {
+            response.status(201).json(data);
+        }
+    })
+    .catch((error) => {
+        response.status(500).json({
+            message: "error FETCHing user by id",
+            error: error.message
+        });
+    })
+});
 
 app.get("/api/users", (request, response) => {
     User.find()
@@ -20,13 +42,10 @@ app.get("/api/users", (request, response) => {
 
 app.get("/api/users/:id", (request, response) => {
     const { id } = request.params;
-    console.log("ID: ", id);
     User.findById(id)
         .then((data) => {
             if (!data) {
-                response.status(404).json({
-                    message: "The user with the specified ID does not exist"
-                })
+                response.status(404).json({ message: "The user with the specified ID does not exist" })
             } else {
                 response.status(200).json(data);
             }
@@ -38,5 +57,52 @@ app.get("/api/users/:id", (request, response) => {
             })
         })
 });
+
+app.delete("/api/users/:id", async (request, response) => {
+    try {
+        const { id } = request.params;
+        const possibleUser = await User.findById(id);
+        if (!possibleUser) {
+            response.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            const deletedUser = await User.remove(possibleUser.id);
+            response.status(200).json(deletedUser);
+        } 
+    } catch (error) {
+        response.status(500).json({
+            message: "error DELETing user by id",
+            error: error.message
+        })
+    }
+});
+
+app.put("/api/users/:id", async (request, response) => {
+    try {
+        const { id } = request.params;
+        const { name, bio } = request.body;
+        const possibleUser = await User.findById(id);
+        if (!possibleUser) {
+            response.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            if ( !name || !bio ) {
+                response.status(400).json({
+                    message: "Please provide name and bio for the user"
+                })
+            } else {
+                const updatedUser = await User.update(id, { name, bio });
+                response.status(200).json(updatedUser);
+            }
+        }
+    } catch (error) {
+        response.status(500).json({
+            message: "error PUTing user by id",
+            error: error.message
+        })        
+    }
+})
 
 module.exports = app;
