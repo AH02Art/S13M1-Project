aa21f83ef6ad243ed71b797d2f3c70491ffd290b
Alex Handley <smithvillescholarsah@gmail.com>
2024-06-21 19:45:28 -0500

    WIP

--- api/server.js
+++ api/server.js
@@ -1,3 +1,36 @@
-// BUILD YOUR SERVER HERE
+const express = require("express");
+const User = require("./users/model.js");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const app = express();
+app.disable("x-powered-by");
+
+app.get("/api/users", (request, response) => {
+    User.find()
+        .then((data) => {
+            response.status(200).json(data);
+            console.log(data);
+        })
+        .catch((error) => {
+            response.status(500).json({
+                message: "error FETCHing users",
+                error: error.message,
+                stack: error.stack
+            });
+        })
+});
+
+app.get("/api/users/:id", (request, response) => {
+    const { id } = request.params;
+    User.findById(id)
+        .then((data) => {
+            response.json(data);
+        })
+        .catch((error) => {
+            response.status(500).json({
+                message: "error FETCHing user by id",
+                error: error.message
+            })
+        })
+});
+
+module.exports = app;
